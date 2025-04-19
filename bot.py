cd5d00fa8eaa496f08331b7b28475e459bdb7e96
EliasVIP99 <140065559+EliasVIP99@users.noreply.github.com>
2025-04-19 14:44:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     application.add_error_handler(error_handler)
 
     @app.route("/")
-def home():
+    def home():
     return "Bot is running!"
 
     application.run_polling()
