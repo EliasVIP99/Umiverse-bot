378e507bb238fddd851f83fe4d6d4b0387db0de3
EliasVIP99 <140065559+EliasVIP99@users.noreply.github.com>
2025-04-19 14:40:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,6 +130,9 @@ if __name__ == "__main__":
     application.add_handler(CallbackQueryHandler(button_click_tracker, block=False))
     application.add_error_handler(error_handler)
 
-    app.route("/")(lambda: "Bot is running!")
+    @app.route("/")
+def home():
+    return "Bot is running!"
+
     application.run_polling()
 
